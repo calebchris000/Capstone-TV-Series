de7a12f3a37cfcd5fb3493528802f19aeef68f84
calebchris000 <calebchris000@gmail.com>
2023-03-16 11:56:38 +0100

    The mock folder with test functions

---
+++ __mocks__/itemCounter.js
@@ -0,0 +1,28 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+const data = 
+`<div class="movieCount">
+<p class='movie'>One</p>
+<p class='movie'>One</p>
+<p class='movie'>One</p>
+<p class='movie'>One</p>
+<p class='movie'>One</p>
+<p class='movie'>One</p>
+<p class='movie'>One</p>
+<p class='movie'>One</p>
+<p class='movie'>One</p>
+<p class='movie'>One</p>
+</div>`
+
+const dom = new JSDOM(data);
+const document = dom.window.document;
+
+const countMovies = () => {
+  const movieCount = document.querySelector('.movieCount');
+  const movies = document.querySelectorAll(".movie");
+  movieCount.innerHTML = movies.length;
+  return movies.length;
+};
+
+module.exports = countMovies;
